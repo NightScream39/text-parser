8bc20fa880d2063bd3bdef9c3f6517554c8b5423
NightScream39 <roman.nightscream333@gmail.com>
2022-08-24 21:02:25 +0300

    cost reduction

--- parser.py
+++ parser.py
@@ -1,30 +1,30 @@
 import re
 
 
+data_array = []
+
+
 def prepare_text():
-    with open('PythonTest.txt') as f:
-        lines = f.readlines()
-    pattern = re.compile(re.escape('#'))
-    with open('PythonTest.txt', 'w') as f:
+    with open('PythonTest.txt') as PT:
+        lines = PT.readlines()
+        pattern = re.compile(re.escape('#'))
         for line in lines:
             result = pattern.search(line)
             if result is None and line != '\n':
-                f.write(line)
+                data_array.append(line)
 
 
 def text_parser():
-    with open('PythonTest.txt') as f:
-        lines = f.readlines()
-        for line in lines:
-            array = line.rstrip().split('\t')
-            array_eng = list(set(array[0].split(" ; ")))
-            array_ru = list(set(array[1].split(" ; ")))
+    for string in data_array:
+        array = string.rstrip().split('\t')
+        array_eng = list(set(array[0].split(" ; ")))
+        array_ru = list(set(array[1].split(" ; ")))
 
-            with open('English.txt', 'a', encoding='utf-8') as Eng, open('Russian.txt', 'a', encoding='utf-8') as Ru:
-                for word in array_eng:
-                    for slovo in array_ru:
-                        Eng.write(word+'\n')
-                        Ru.write(slovo+'\n')
+        with open('English.txt', 'a', encoding='utf-8') as Eng, open('Russian.txt', 'a', encoding='utf-8') as Ru:
+            for word in array_eng:
+                for slovo in array_ru:
+                    Eng.write(word+'\n')
+                    Ru.write(slovo+'\n')
 
 
 if __name__ == "__main__":
